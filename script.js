8a744a09f441066951b03c9212e276118c53dcad
yagicchi <yagitaka0315@gmail.com>
2026-05-14 23:48:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,480 @@
+const tweets = {
+  "2025-11-21": [
+    {
+      title: "「北海道の昆布ぅ〜」で自分で笑っちゃう...",
+      url: "https://x.com/yagi_ya0315/status/1991898720959635677?s=20"
+    }
+  ],
+  "2025-11-22": [
+    {
+      title: "目にも止まらぬ高速パンチ👊🏻💥...",
+      url: "https://x.com/yagi_ya0315/status/1992275076079853572?s=20"
+    }
+  ],
+  "2025-11-23": [
+    {
+      title: "おゆそん...",
+      url: "https://x.com/yagi_ya0315/status/1992618898739593492?s=20"
+    }
+  ],
+"2025-11-24": [
+    {
+      title: "関↗️西↘️弁(エセ)...",
+      url: "https://x.com/yagi_ya0315/status/1992984940418670934?s=20"
+    }
+  ],
+"2025-11-25": [
+    {
+      title: "ご当地ピカチュウをたくさん抱えたおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1993267699238277484?s=20"
+    }
+  ],
+"2025-11-26": [
+    {
+      title: "たいやきを食べるしらいしをりせぴと一緒に盛り上げる...",
+      url: "https://x.com/yagi_ya0315/status/1993692702534480138?s=20"
+    }
+  ],
+"2025-11-27": [
+    {
+      title: "寝ぐせ爆発ファンタスティック...",
+      url: "https://x.com/yagi_ya0315/status/1994072884667371911?s=20"
+    }
+  ],
+"2025-11-28": [
+    {
+      title: "B.L.Tの連載の可愛いすぎるおゆいとフリフリのお洋服...",
+      url: "https://x.com/yagi_ya0315/status/1994430060976382262?s=20"
+    }
+  ],
+"2025-11-29": [
+    {
+      title: "ちょっといじけた感じのおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1994796973967511961?s=20"
+    }
+  ],
+"2025-11-30": [
+    {
+      title: "カワコレマッチ未だ無敗のおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1995175128939053384?s=20"
+    }
+  ],
+"2025-12-01": [
+    {
+      title: "プライベートでも釣りをするおゆい🐟...",
+      url: "https://x.com/yagi_ya0315/status/1995538119752581179?s=20"
+    }
+  ],
+"2025-12-02": [
+    {
+      title: "フラスタに溶け込むおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1995888635745632404?s=20"
+    }
+  ],
+"2025-12-03": [
+    {
+      title: "自称パンチネキ、恐ろしく速いラッシュ...",
+      url: "https://x.com/yagi_ya0315/status/1996249241023594707?s=20"
+    }
+  ],
+"2025-12-04": [
+    {
+      title: "わんわん🐶髪色も相まってめっちゃ高貴なワンちゃんに見える...",
+      url: "https://x.com/yagi_ya0315/status/1996631116040843306?s=20"
+    }
+  ],
+"2025-12-05": [
+    {
+      title: "ガチ恋不可避の距離感のおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1996980799770640827?s=20"
+    }
+  ],
+"2025-12-06": [
+    {
+      title: "椅子取りゲーム強いおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1997382118054547477?s=20"
+    }
+  ],
+"2025-12-07": [
+    {
+      title: "印が組めないおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1997694308804350013?s=20"
+    }
+  ],
+"2025-12-08": [
+    {
+      title: "剣に対して拳で立ち向かう...",
+      url: "https://x.com/yagi_ya0315/status/1998081875978682550?s=20"
+    }
+  ],
+"2025-12-09": [
+    {
+      title: "つままれてジタバタするおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1998423627272978441?s=20"
+    }
+  ],
+"2025-12-10": [
+    {
+      title: "お休みのｱﾕﾁｬﾝの想いをｵﾀﾞﾝｺﾞｶﾗﾋﾟﾛﾋﾟﾛﾃﾞﾃﾙﾖﾍｱｰでステージに連れて行ってくれたおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1998793342033998124?s=20"
+    }
+  ],
+"2025-12-11": [
+    {
+      title: "ジェリーの真似をするおゆい。表情もバッチリ表現...",
+      url: "https://x.com/yagi_ya0315/status/1999159076786512143?s=20"
+    }
+  ],
+"2025-12-12": [
+    {
+      title: "撮可のルール説明をするおゆい...",
+      url: "https://x.com/yagi_ya0315/status/1999487635371508066?s=20"
+    }
+  ],
+"2025-12-13": [
+    {
+      title: "本当にしっぽみたいで可愛いサイドテール...",
+      url: "https://x.com/yagi_ya0315/status/1999880304635629791?s=20"
+    }
+  ],
+"2025-12-14": [
+    {
+      title: "ポポポイおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2000236438965846112?s=20"
+    }
+  ],
+"2025-12-15": [
+    {
+      title: "バッジでコートの前を留めるおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2000613488495083827?s=20"
+    }
+  ],
+"2025-12-16": [
+    {
+      title: "誕生日近い&語学堪能で共通点多めなノエおゆペア...",
+      url: "https://x.com/yagi_ya0315/status/2000958512978125178?s=20"
+    }
+  ],
+"2025-12-17": [
+    {
+      title: "🩵おゆい→まなふぃ、🩷なつかちゃん→かれんたんで渡したかったのに立ち位置が逆になっちゃってテンパるおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2001324135088336964?s=20"
+    }
+  ],
+"2025-12-18": [
+    {
+      title: "真ん中の位置に来るまでのステップ激カワ、3分半ず〜っと動き回ってて可愛かった...",
+      url: "https://x.com/yagi_ya0315/status/2001704748786024933?s=20"
+    }
+  ],
+"2025-12-19": [
+    {
+      title: "しらいしにいじわるされるおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2002035092370411651?s=20"
+    }
+  ],
+"2025-12-20": [
+    {
+      title: "とぼけるおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2002427600476786806?s=20"
+    }
+  ],
+"2025-12-21": [
+    {
+      title: "カップラーメンおゆいと2代目湯切りネキ(？)...",
+      url: "https://x.com/yagi_ya0315/status/2002779092383580305?s=20"
+    }
+  ],
+"2025-12-22": [
+    {
+      title: "(衣装展の衣装を触るなつかちゃんに)あ〜コラｺﾗｺﾗ...",
+      url: "https://x.com/yagi_ya0315/status/2003148029831037172?s=20"
+    }
+  ],
+"2025-12-23": [
+    {
+      title: "獲ったど〜!!(しらいし生誕ver.)...",
+      url: "https://x.com/yagi_ya0315/status/2003521099808792895?s=20"
+    }
+  ],
+"2025-12-24": [
+    {
+      title: "ﾋﾟﾖｰﾝ...",
+      url: "https://x.com/yagi_ya0315/status/2003872809538539792?s=20"
+    }
+  ],
+"2025-12-25": [
+    {
+      title: "おゆうさサンタ🐰...",
+      url: "https://x.com/yagi_ya0315/status/2004228658450641130?s=20"
+    }
+  ],
+"2025-12-26": [
+    {
+      title: "「雪が降って嬉しい顔」対決で何故か自ら雪だるまになるおゆい☃️...",
+      url: "https://x.com/yagi_ya0315/status/2004582215565627843?s=20"
+    }
+  ],
+"2025-12-27": [
+    {
+      title: "おこおこも全部KAWAIIおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2004984426107990380?s=20"
+    }
+  ],
+"2025-12-28": [
+    {
+      title: "みんなからGeeの合いの手を集めるおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2005328423233978872?s=20"
+    }
+  ],
+"2025-12-29": [
+    {
+      title: "エセ(？)関西弁おゆい...",
+      url: "https://x.com/yagi_ya0315/status/2005682520688582755?s=20"
+    }
+  ],
+"2025-12-30": [
+    {
+      title: "いつもよりちょっとオトナなおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2006064759540793588?s=20"
+    }
+  ],
+"2025-12-31": [
+    {
+      title: "振袖、いや、フリル袖のおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2006395337062408494?s=20"
+    }
+  ],
+"2026-01-01": [
+    {
+      title: "美しいお辞儀🎍...",
+      url: "https://x.com/yagi_ya0315/status/2006786259923140895?s=20"
+    }
+  ],
+"2026-01-02": [
+    {
+      title: "おゆうさサンタカムバック(助かる)...",
+      url: "https://x.com/yagi_ya0315/status/2007153893252468802?s=20"
+    }
+  ],
+"2026-01-03": [
+    {
+      title: "お餅をつくおゆなつペア、杵を持つことでアイスクライマー感がマシマシに...",
+      url: "https://x.com/yagi_ya0315/status/2007499042448912883?s=20"
+    }
+  ],
+"2026-01-04": [
+    {
+      title: "動きづらそうな振袖&草履でもしっかり踊れるおゆい...",
+      url: "https://x.com/yagi_ya0315/status/2007868094279274770?s=20"
+    }
+  ],
+"2026-01-05": [
+    {
+      title: "大吉!!YAKIMOCHI !!...",
+      url: "https://x.com/yagi_ya0315/status/2008232928661434844?s=20"
+    }
+  ],
+"2026-01-06": [
+    {
+      title: "大変身✨するおゆい、変身の前も後も可愛いのでこれはもうプリキュアとかの類だ！！...",
+      url: "https://x.com/yagi_ya0315/status/2008582155275039078?s=20"
+    }
+  ],
+"2026-01-07": [
+    {
+      title: "ｱﾕﾁｬﾝ生誕衣装のおゆい、ネクタイおゆい激レアすぎてやばい👔...",
+      url: "https://x.com/yagi_ya0315/status/2008949682870116354?s=20"
+    }
+  ],
+"2026-01-08": [
+    {
+      title: "ツインテおゆいwith生誕広告...",
+      url: "https://x.com/yagi_ya0315/status/2009317648543445155?s=20"
+    }
+  ],
+"2026-01-09": [
+    {
+      title: "プレゼント箱から出てくるおゆい🎁プレゼントでおゆいが届くとか最高すぎる...",
+      url: "https://x.com/yagi_ya0315/status/2009667219626877427?s=20"
+    }
+  ]
+
+
+};
+
+const calendar = document.getElementById("calendar");
+const yearSelect = document.getElementById("yearSelect");
+const monthSelect = document.getElementById("monthSelect");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentYear = 2026;
+let currentMonth = 05;
+
+// 年プルダウン作成
+for (let year = 2025; year <= 2026; year++) {
+
+  const option = document.createElement("option");
+
+  option.value = year;
+
+  option.textContent = `${year}年`;
+
+  yearSelect.appendChild(option);
+
+}
+
+// カレンダー描画
+function renderCalendar(year, month) {
+
+  calendar.innerHTML = "";
+
+  yearSelect.value = year;
+  monthSelect.value = month;
+
+  const firstDay = new Date(year, month - 1, 1);
+
+  const lastDay = new Date(year, month, 0);
+
+  const startWeekDay = firstDay.getDay();
+
+  const daysInMonth = lastDay.getDate();
+
+  // 空白マス
+  for (let i = 0; i < startWeekDay; i++) {
+
+    const emptyBox = document.createElement("div");
+
+    emptyBox.classList.add("day", "empty");
+
+    calendar.appendChild(emptyBox);
+
+  }
+
+  // 日付生成
+  for (let day = 1; day <= daysInMonth; day++) {
+
+    const dayBox = document.createElement("div");
+
+    dayBox.classList.add("day");
+
+    const weekDay = new Date(year, month - 1, day).getDay();
+
+    // 日曜
+    if (weekDay === 0) {
+
+      dayBox.classList.add("sunday");
+
+    }
+
+    // 土曜
+    if (weekDay === 6) {
+
+      dayBox.classList.add("saturday");
+
+    }
+
+    // yyyy-mm-dd
+    const dateKey =
+      `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
+
+    // ツイートある日
+    if (tweets[dateKey]) {
+
+      dayBox.classList.add("hasTweet");
+
+    }
+
+    // 日付表示
+    dayBox.innerHTML = `<div class="date">${day}</div>`;
+
+    // ツイート表示
+    if (tweets[dateKey]) {
+
+      tweets[dateKey].forEach(tweet => {
+
+        const link = document.createElement("a");
+
+        link.href = tweet.url;
+
+        link.textContent = tweet.title;
+
+        link.target = "_blank";
+
+        dayBox.appendChild(link);
+
+      });
+
+    }
+
+    calendar.appendChild(dayBox);
+
+  }
+
+}
+
+// 前の月
+prevBtn.addEventListener("click", () => {
+
+  currentMonth--;
+
+  if (currentMonth === 0) {
+
+    currentMonth = 12;
+    currentYear--;
+
+  }
+
+  if (currentYear < 2025) {
+
+    currentYear = 2025;
+    currentMonth = 1;
+
+  }
+
+  renderCalendar(currentYear, currentMonth);
+
+});
+// 次の月
+nextBtn.addEventListener("click", () => {
+
+  currentMonth++;
+
+  if (currentMonth === 13) {
+
+    currentMonth = 1;
+    currentYear++;
+
+  }
+
+  if (currentYear > 2026) {
+
+    currentYear = 2026;
+    currentMonth = 12;
+
+  }
+
+  renderCalendar(currentYear, currentMonth);
+
+});
+// 年変更
+yearSelect.addEventListener("change", () => {
+
+  currentYear = Number(yearSelect.value);
+
+  renderCalendar(currentYear, currentMonth);
+
+});
+
+// 月変更
+monthSelect.addEventListener("change", () => {
+
+  currentMonth = Number(monthSelect.value);
+
+  renderCalendar(currentYear, currentMonth);
+
+});
+
+// 初回表示
+renderCalendar(currentYear, currentMonth);
